5b46427e2f4508fcb32314c8bd4cf383c0d0642c
ClemBesacier <clementinebesacier@yahoo.fr>
2019-09-12 16:15:21 +0200

    update webpack

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,7 +2,7 @@
 //      ANCHOR -- require and imports
 // ---------------------------------------- //
 
-
+const css = require('../styles/index.scss')
 
 // ---------------------------------------- //
 //      ANCHOR -- dom ready (just need to load the files)
